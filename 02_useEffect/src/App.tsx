102ec7ae01ba902fba670e3cbcaa123e4bdbb314
nopphawong.n <nopphawong.n@gmail.com>
2023-04-19 14:30:59 +0700

    useEffect

--- 02_useEffect/src/App.tsx
+++ 02_useEffect/src/App.tsx
@@ -1,7 +1,45 @@
+import { Component } from 'react'
 import { useEffect, useState } from 'react'
 import logo from './assets/react.svg'
 import axios from 'axios'
 import { User } from './models/User'
+
+interface IState {
+  hasError: boolean
+  users: User[]
+}
+
+// class App extends Component<{}, IState> {
+//   constructor(props: any) {
+//     super(props)
+//     this.state = {
+//       hasError: false,
+//       users: [],
+//     }
+//   }
+
+//   componentDidMount(): void {
+//     axios
+//       .get('https://jsonplaceholder.typicode.com/users')
+//       .then(({ data }) => this.setState({ users: data }))
+//   }
+
+//   render() {
+//     return (
+//       <div className='container mx-auto'>
+//         <div className='flex flex-col justify-center items-center gap-y-6 h-screen text-center'>
+//           <img src={logo} className='w-40 cursor-pointer' alt='react logo' />
+//           <span>Error: {JSON.stringify(this.state.hasError)}</span>
+//           <ul>
+//             {this.state.users.map((user) => (
+//               <li key={user.id}>{user.name}</li>
+//             ))}
+//           </ul>
+//         </div>
+//       </div>
+//     )
+//   }
+// }
 
 const App = () => {
   const [hasError, setErrors] = useState<boolean>(false)
@@ -9,9 +47,10 @@ const App = () => {
 
   async function getUsers() {
     try {
-      const { data } = await axios.get('https://randomuser.me/api/')
-      console.log(data.results)
-      setUsers(data.results)
+      const { data } = await axios.get(
+        'https://jsonplaceholder.typicode.com/users'
+      )
+      setUsers(data)
     } catch (err) {
       setErrors(true)
     }
@@ -25,8 +64,12 @@ const App = () => {
     <div className='container mx-auto'>
       <div className='flex flex-col justify-center items-center gap-y-6 h-screen text-center'>
         <img src={logo} className='w-40 cursor-pointer' alt='react logo' />
-        <span>{JSON.stringify(`${users[0]?.name.first} ${users[0]?.name.last}`)}</span>
-        <span>{JSON.stringify(hasError)}</span>
+        <span>Error: {JSON.stringify(hasError)}</span>
+        <ul>
+          {users.map((user) => (
+            <li key={user.id}>{user.name}</li>
+          ))}
+        </ul>
       </div>
     </div>
   )
